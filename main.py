ef719351418d0ddd39c99d2a861d620b9afe5d8e
88brianw <88brianw@berkeley.edu>
2026-03-05 14:31:28 -0800

    new port

--- main.py
+++ main.py
@@ -180,4 +180,5 @@ Comments:
     return jsonify(roasted)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
